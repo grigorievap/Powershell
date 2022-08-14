be1dd84395534baac91ed586385a2941f7126bbc
Klaus <grigorievap@ya.ru>
2022-08-14 17:09:23 +0300

    Добавлены команды

--- Some commands.ps1
+++ Some commands.ps1
@@ -1,6 +1,7 @@
 # Поиск по истории комманд
-Get-Content (Get-PSReadLineOption | select -ExpandProperty HistorySavePath) | Select terra
+Get-Content (Get-PSReadLineOption | Select-Object -ExpandProperty HistorySavePath) | Select-Object terra
 # Настройки команд
-Get-PSReadlineOption | select HistoryNoDuplicates, MaximumHistoryCount, HistorySearchCursorMovesToEnd, HistorySearchCaseSensitive, HistorySavePath, HistorySaveStyle
+Get-PSReadlineOption | Select-Object HistoryNoDuplicates, MaximumHistoryCount, HistorySearchCursorMovesToEnd, HistorySearchCaseSensitive, HistorySavePath, HistorySaveStyle
 # Установка параметров
 Set-PSReadlineOption -MaximumHistoryCount 10000
+#
